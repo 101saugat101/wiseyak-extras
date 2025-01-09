4ce06e9488623bcb41955655946de032436b2c84
saugat thapa <saugat.thapa@wiseyak.com>
2025-01-09 14:30:16 +0545

    summarization feature with local llm

--- fastapi_file_handeling/llm_summarize.py
+++ fastapi_file_handeling/llm_summarize.py
@@ -1,6 +1,30 @@
 import os
 import json
+from llama_cpp import Llama
+
+#setuping the lama and mistral
+llm = Llama(model_path="models/mistral-7b-instruct.Q4_K_M.ggmlv3.q4_0.bin",chat_format="llama-2",cpu_threads=os.cpu_count(),n_ctx=4096)
+
+def summarize_text(transciption):
+    try:
+        result=llm.create_chat_completion(
+            messages=[
+                {"role": "system", "content":"""you are assistant that recieves transciption and generates summary
+                please generate summary in format
+                {
+                "summary":"summary of transciption",
+                "discription":"description of transciption"
+                }"""},
+                {"role": "user", "content":transciption}
+            ])
+
+        return json.loads(result)
+    except Exception as e:
+        return None,str(e)
 
 
 
-def summarize_text(text):
+
+
+
+
